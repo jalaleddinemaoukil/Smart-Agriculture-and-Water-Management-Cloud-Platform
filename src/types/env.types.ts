9fc75f3aefd7e0bae414b4900c5c07cd3cab20a7
Jalal Eddine <Jalaleddinemaoukil@gmail.com>
2025-10-17 17:55:16 +0100

    redeploy

--- src/types/env.types.ts
+++ src/types/env.types.ts
@@ -2,8 +2,8 @@ export {};
 
 declare global {
   interface ImportMetaEnv {
-    readonly REACT_APP_API_KEY: string;
-    readonly REACT_APP_API_BASE_URL: string;
+    readonly VITE_API_KEY: string;
+    readonly VITE_API_BASE_URL: string;
     readonly AZURE_SUBSCRIPTION_ID: string;
     readonly AZURE_TENANT_ID: string;
     readonly EVENTHUB_CONNECTION_STR: string;
